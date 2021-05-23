dda82bf227fc7d52569a0417cf1f40116cf32d83
Chreim Jean Pierre <jeanpierre.chreim@isae.edu.lb>
2021-05-23 19:10:55 +0300

    Creation de nos 1er fichiers

---
+++ troisiemeEssai/person.java
@@ -0,0 +1,34 @@
+
+/**
+ * Write a description of class person here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class person
+{
+    // instance variables - replace the example below with your own
+    private int x;
+    private String firstName;
+    private String lastName;
+    /**
+     * Constructor for objects of class person
+     */
+    public person()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
